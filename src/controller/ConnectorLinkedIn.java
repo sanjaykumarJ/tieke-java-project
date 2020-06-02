d6dc5f6e8bb2562ceaa8881fd9fa04f892b8f43a
sk <balasanjaykumar@gmail.com>
2020-06-03 00:01:56 +0530

    Update ConnectorLinkedIn.java

--- src/controller/ConnectorLinkedIn.java
+++ src/controller/ConnectorLinkedIn.java
@@ -25,7 +25,7 @@ public class ConnectorLinkedIn {
                     .cookies(connect.cookies())
                     .data("loginCsrfParam", csrfValue.attr("value"))
                     .data("session_key", "balasanjaykumar@gmail.com")
-                    .data("session_password", "sanjjjawa@95")
+                    .data("session_password", "")
                     .userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36")
                     .method(Method.POST)
                     .followRedirects(true)
